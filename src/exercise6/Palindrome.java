7960b143bd38c536138d751b72bf34783130870f
Gretien <gretien.kolev@fdiba-tu.sofia.bg>
2020-11-05 07:21:35 +0000

    a

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -5,5 +5,24 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+        int copy = number;
+        int revnumber = 0;
+
+        while(copy!=0){
+          int digit = copy % 10;
+          copy /= 10;
+          revnumber *= 10;
+          revnumber += digit;
+
+        }
+        if ( revnumber == number){
+          System.out.println("Palindrome: true");
+
+        }else{
+          System.out.println("Palindrome: false");
+
+        }
+
     }
 }

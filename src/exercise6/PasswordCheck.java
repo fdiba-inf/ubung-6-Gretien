423e2098d69821364ecb3333f89c6117cd018d02
Gretien <gretien.kolev@fdiba-tu.sofia.bg>
2020-11-05 07:53:03 +0000

    a

--- src/exercise6/PasswordCheck.java
+++ src/exercise6/PasswordCheck.java
@@ -5,5 +5,33 @@ import java.util.Scanner;
 public class PasswordCheck {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        while(true){
+          String password = input.nextLine();
+          boolean valid = true;
+          int count = 0;
+
+          if(password.length() < 8 ){
+            valid = false;
+            continue;
+          }else{
+            for( int i = 0; i < password.length(); i++){
+              if(Character.isDigit(password.charAt(i))){
+                count++;
+              }
+              if(!Character.isLetterOrDigit(password.charAt(i))){
+                valid = false;
+                break;
+
+              }
+            }
+          }
+          if(count < 2 ){
+            valid = false;
+          }
+          if(valid= true){
+            System.out.println("Password valid!");
+            break;
+          }
+        }
     }
 }
